66db93b966a2a5f797c7da4ac134b424334104c3
Frère Jules <73660947+JurleBro@users.noreply.github.com>
2023-03-07 15:12:34 +0100

    ant, maven et gradle

--- src/main/java/com/ubo/tp/twitub/TwitubLauncher.java
+++ src/main/java/com/ubo/tp/twitub/TwitubLauncher.java
@@ -15,6 +15,7 @@ public class TwitubLauncher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Lancement de l'application");
 		Twitub twitub = new Twitub();
 		twitub.show();
 	}
