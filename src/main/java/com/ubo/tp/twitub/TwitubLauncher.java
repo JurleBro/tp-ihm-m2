4cb444307c6ab06e8faea082bff3a579b7299942
Frère Jules <73660947+JurleBro@users.noreply.github.com>
2023-03-08 11:25:21 +0100

    test build on push

--- src/main/java/com/ubo/tp/twitub/TwitubLauncher.java
+++ src/main/java/com/ubo/tp/twitub/TwitubLauncher.java
@@ -15,7 +15,6 @@ public class TwitubLauncher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Lancement de l'application");
 		Twitub twitub = new Twitub();
 		twitub.show();
 	}
